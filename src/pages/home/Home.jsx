11f1af12bcf53d53d485bc3b548b204e95486352
Adam Abdillah <adamabdillah20@gmail.com>
2022-07-05 01:29:15 +0700

    create featured component for reservasi and adding some properties like rating and places

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import Featured from '../../component/featured/Featured';
 import { Header } from '../../component/header/Header';
 import { Navbar } from '../../component/nav/Navbar';
+import { Properties } from '../../component/properties/Properties';
+import { PropertyList } from '../../component/propertyList/PropertyList';
 import './home.css';
 
 export const Home = () => {
@@ -8,6 +11,13 @@ export const Home = () => {
     <div>
       <Navbar />
       <Header />
+      <div className="home__container">
+        <Featured />
+        <h1 className="home_title">Browse by property type</h1>
+        <PropertyList />
+        <h1 className="home_title">Homes guests love</h1>
+        <Properties />
+      </div>
     </div>
   )
 }
